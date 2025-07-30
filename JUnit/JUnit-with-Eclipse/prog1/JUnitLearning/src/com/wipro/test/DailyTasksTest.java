a3992bc7cb08e156a33eec47cd7067e566f4e505
lord-cyclone100 <sang66deb@gmail.com>
2025-07-30 06:52:08 +0530

    Rectified code

--- JUnit/JUnit-with-Eclipse/prog1/JUnitLearning/src/com/wipro/test/DailyTasksTest.java
+++ JUnit/JUnit-with-Eclipse/prog1/JUnitLearning/src/com/wipro/test/DailyTasksTest.java
@@ -2,6 +2,7 @@ package com.wipro.test;
 
 //import static org.junit.Assert;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertArrayEquals;
 
 import org.junit.Test;
 
@@ -13,9 +14,27 @@ public class DailyTasksTest {
 	public void testDoStringConcat() {
 		DailyTasks dt = new DailyTasks();
 		String actual = dt.doStringConcat("Hello", "World");
-		String expected = "HelloWorld";
+		String expected = "Hello World";
 		assertEquals(expected, actual);
 //		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testSortValues() {
+		DailyTasks dt = new DailyTasks();
+		int[] arr = {3,5,1,4,2};
+		int[] actual = dt.sortValues(arr);
+		int[] expected = {1,2,3,4,5};
+		assertArrayEquals(expected, actual);
+	}
+	
+	@Test
+	public void testCheckPresence() {
+		DailyTasks dt = new DailyTasks();
+		String str = "Java";
+		boolean actual = dt.checkPresence(str, "a");
+		boolean expected = true;
+		assertEquals(expected,actual);
 	}
 
 }
